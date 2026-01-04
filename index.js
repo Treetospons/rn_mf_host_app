1629efa5719c91cd01c3c6efcc7f759f7b1c3a5c
treetospon <treetospons@gmail.com>
2026-01-04 23:09:06 +0700

    Code Signing

--- index.js
+++ index.js
@@ -5,16 +5,33 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
-import { ScriptManager, Script } from "@callstack/repack/client";
+import { ScriptManager, Script, Federated } from "@callstack/repack/client";
 
 console.log('before');
 
-ScriptManager.shared.addResolver(async (scriptId) => {
+const containers = {
+    mini_app1: "http://localhost:3011/[name][ext]",
+};
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
     // if (scriptId === "mini_app1") {
-    return {
-        url: Script.getDevServerURL(scriptId),
-    };
+    // return {
+    //     url: Script.getDevServerURL(scriptId),
+    // };
     // }
+
+    const resolveURL = Federated.createURLResolver({ containers });
+
+    const url = resolveURL(scriptId, caller);
+    console.log(url);
+    if (url) {
+        return {
+            url,
+            query: { platform: Platform.OS },
+            // verifyScriptSignature: __DEV__ ? "off" : "strict",
+            verifyScriptSignature: "strict",
+        };
+    }
 })
 
 AppRegistry.registerComponent(appName, () => App);
--- rspack.config.mjs
+++ rspack.config.mjs
@@ -11,57 +11,66 @@ const __dirname = path.dirname(__filename);
  * Learn about Rspack configuration: https://rspack.dev/config/
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
-
-export default Repack.defineRspackConfig({
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(),
-  },
-  output: {
-    uniqueName: 'host_app',
-    clean: true,
-    filename: "index.bundle",
-    chunkFilename: "[name].chunk.bundle",
-    path: "[context]/build/generated/[platform]",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[cm]?[jt]sx?$/,
-        type: 'javascript/auto',
-        use: {
-          loader: '@callstack/repack/babel-swc-loader',
-          parallel: true,
-          options: {},
+const config = (_env) => {
+  const { mode } = _env
+  console.log(mode)
+  return Repack.defineRspackConfig({
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    output: {
+      uniqueName: 'host_app',
+      clean: true,
+      filename: "index.bundle",
+      chunkFilename: "[name].chunk.bundle",
+      path: "[context]/build/generated/[platform]",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[cm]?[jt]sx?$/,
+          type: 'javascript/auto',
+          use: {
+            loader: '@callstack/repack/babel-swc-loader',
+            parallel: true,
+            options: {},
+          },
         },
-      },
-      ...Repack.getAssetTransformRules(),
+        ...Repack.getAssetTransformRules(),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      new Repack.plugins.CodeSigningPlugin({
+        enabled: true,
+        privateKeyPath: "./code-signing.pem",
+      }),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'host_app',
+        remotes: {
+          mini_app1: 'mini_app1@http://localhost:3011/android/mf-manifest.json',
+        },
+        shared: {
+          react: { singleton: true, eager: true, requiredVersion: '19.1.0' },
+          'react-native': { singleton: true, eager: true, requiredVersion: '0.80.0' },
+          'react-native-screens': { singleton: true, eager: true },
+          'react-native-safe-area-context': { singleton: true, eager: true },
+          '@react-native/new-app-screen': { singleton: true, eager: true },
+          '@react-navigation/native': { singleton: true, eager: true },
+          '@react-navigation/native-stack': { singleton: true, eager: true },
+          '@react-navigation/elements': { singleton: true, eager: true },
+          '@react-navigation/bottom-tabs': { singleton: true, eager: true },
+          '@react-native-masked-view/masked-view': { singleton: true, eager: true },
+          'react-native-vision-camera': { singleton: true, eager: true },
+          'react-native-permissions': { singleton: true, eager: true },
+          'react-native-config': { singleton: true, eager: true },
+        },
+        dts: false
+      })
     ],
-  },
-  plugins: [
-    new Repack.RepackPlugin(),
-    new Repack.plugins.ModuleFederationPluginV2({
-      name: 'host_app',
-      remotes: {
-        mini_app1: 'mini_app1@http://localhost:3011/android/mf-manifest.json',
-      },
-      shared: {
-        react: { singleton: true, eager: true, requiredVersion: '19.1.0' },
-        'react-native': { singleton: true, eager: true, requiredVersion: '0.80.0' },
-        'react-native-screens': { singleton: true, eager: true },
-        'react-native-safe-area-context': { singleton: true, eager: true },
-        '@react-native/new-app-screen': { singleton: true, eager: true },
-        '@react-navigation/native': { singleton: true, eager: true },
-        '@react-navigation/native-stack': { singleton: true, eager: true },
-        '@react-navigation/elements': { singleton: true, eager: true },
-        '@react-navigation/bottom-tabs': { singleton: true, eager: true },
-        '@react-native-masked-view/masked-view': { singleton: true, eager: true },
-        'react-native-vision-camera': { singleton: true, eager: true },
-        'react-native-permissions': { singleton: true, eager: true },
-        'react-native-config': { singleton: true, eager: true },
-      },
-      dts: false
-    })
-  ],
-});
+  });
+}
+
+export default config
